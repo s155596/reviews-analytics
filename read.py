687565e0910899409e9ba9c60e340aa94304fdd6
Tom Chang <s155596@gmail.com>
2020-07-20 21:12:27 +0800

    clac avg length of reviews

--- read.py
+++ read.py
@@ -6,6 +6,9 @@ with open('reviews.txt','r') as f:
 		count += 1 
 		if count % 1000 == 0: # 如果數到1000的倍數
 			print(len(data)) # 把data的筆數印出來
-print(data[0])
-print('---------')
-print(data[1])
+print('檔案讀取完了, 總共有', len(data),'筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言的平均長度為', sum_len/len(data))
